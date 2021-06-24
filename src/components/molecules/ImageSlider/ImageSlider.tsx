38515bd5ed0286f369c953e5f58b0ad2174d5916
Dipen Parikh <dipenparikh9@icloud.com>
2021-06-24 19:43:06 -0400

    make imageSlider height dynamic as per its parent container

--- src/components/molecules/ImageSlider/ImageSlider.tsx
+++ src/components/molecules/ImageSlider/ImageSlider.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 
 import './ImageSlider.scss';
 
@@ -8,18 +8,18 @@ type ImageSliderProps = {
     id: string;
     name: string;
     images: string[];
-
+    style: CSSProperties;
 }
 
-const ImageSlider = ( {id, name, images}: ImageSliderProps ) :JSX.Element => {
+const ImageSlider = ( {id, name, images, style}: ImageSliderProps ) :JSX.Element => {
 
     function renderImageItems(images: string[]) {
         const imageItems = images.map( image => { 
             const imgSrc = image;
             return (
-            <Carousel.Item key={imgSrc}>
+            <Carousel.Item key={imgSrc} className="imageWrapper">
                 <img
-                    className="d-block w-100 imageContainer"
+                    className="d-block w-100 image"
                     src = {imgSrc}
                     alt="First slide"
                 />
@@ -30,8 +30,8 @@ const ImageSlider = ( {id, name, images}: ImageSliderProps ) :JSX.Element => {
     }
 
     return (
-        <div id={id}>
-            <Carousel indicators={false}>
+        <div id={id} className="imageSliderContainer" style={style}>
+            <Carousel indicators={false} className="imageSlider">
                 {renderImageItems(images)}
             </Carousel>
         </div>
