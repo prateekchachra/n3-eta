8be8a2d98e66df3e31c9f009f1f6fa0c8096f958
Dipen Parikh <dipenparikh9@icloud.com>
2021-06-29 22:10:55 -0400

    use sampleData from JSON file to display product list on Homepage

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -7,13 +7,17 @@ import PageTemplate from '../../components/templates/PageTemplate';
 import ImageSlider from '../../components/molecules/ImageSlider/ImageSlider';
 import ProductCard from '../../components/organisms/ProductCard/ProductCard';
 
+import JsonProductList from '../../assets/sampleData/Products.json';
+
 const banners = [
     "https://images.unsplash.com/photo-1467043237213-65f2da53396f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
   ];
 
 const HomePage = () => {
     const history = useHistory();
-
+    const productList = [...JSON.parse(JSON.stringify(JsonProductList)).products].filter(product => product.isTrending);
+    const categories = [...JSON.parse(JSON.stringify(JsonProductList)).categories];
+    
     function onProductCardClickHandler(event: any) {
         event.preventDefault();
         history.push("/product-detail");
@@ -30,37 +34,32 @@ const HomePage = () => {
     //TODO: add 'listOfProductsByCategoryMap' as an arg, iterator through each item
     function renderProductListColumns() {
         const productListColumnRendererArray: ReactNodeArray = [];
-        [1,2,3].forEach(element => {
+        categories.forEach(category => {
             productListColumnRendererArray.push(
-                <div className="productListColumnContainer">
+                <div className="productListColumnContainer" key={category}>
                     <div className="textBannerContainer">
-                        <h4 className="textBannerTitle">Categories to bag</h4>
+                        <h4 className="textBannerTitle">{category}</h4>
                         <a href="">
                             <h4 className="textBannerTitleLink">View All</h4>
                         </a>
                     </div>
                     <div className="productListContainer">
-                        <ProductCard productTitle="Jack & Jones T-Shirt" price={300} discountPercent={50} imgs={banners} buyNowHandler={(e) => {console.log("Buy Now Clicked")}} addToCartHandler={(e) => {console.log("Add to Cart Clicked")}}
-                            onClickHandler={onProductCardClickHandler}
-                        />
-                        <ProductCard productTitle="Jack & Jones T-Shirt" price={300} discountPercent={50} imgs={banners} buyNowHandler={(e) => {console.log("Buy Now Clicked")}} addToCartHandler={(e) => {console.log("Add to Cart Clicked")}}
-                        onClickHandler={onProductCardClickHandler}
-                        />
-                        <ProductCard productTitle="Jack & Jones T-Shirt" price={300} discountPercent={50} imgs={banners} buyNowHandler={(e) => {console.log("Buy Now Clicked")}} addToCartHandler={(e) => {console.log("Add to Cart Clicked")}}
-                            onClickHandler={onProductCardClickHandler}
-                        />
-                        <ProductCard productTitle="Jack & Jones T-Shirt" price={300} discountPercent={50} imgs={banners} buyNowHandler={(e) => {console.log("Buy Now Clicked")}} addToCartHandler={(e) => {console.log("Add to Cart Clicked")}}
-                            onClickHandler={onProductCardClickHandler}
-                        />
-                        <ProductCard productTitle="Jack & Jones T-Shirt" price={300} discountPercent={50} imgs={banners} buyNowHandler={(e) => {console.log("Buy Now Clicked")}} addToCartHandler={(e) => {console.log("Add to Cart Clicked")}}
-                            onClickHandler={onProductCardClickHandler}
-                        />
-                        <ProductCard productTitle="Jack & Jones T-Shirt" price={300} discountPercent={50} imgs={banners} buyNowHandler={(e) => {console.log("Buy Now Clicked")}} addToCartHandler={(e) => {console.log("Add to Cart Clicked")}}
-                        onClickHandler={onProductCardClickHandler}
-                        />
-                        <ProductCard productTitle="Jack & Jones T-Shirt" price={300} discountPercent={50} imgs={banners} buyNowHandler={(e) => {console.log("Buy Now Clicked")}} addToCartHandler={(e) => {console.log("Add to Cart Clicked")}}
-                            onClickHandler={onProductCardClickHandler}
-                        />
+                        {   productList &&
+                            productList.map((product: any) => {
+
+                                return (<ProductCard key={product.id}
+                                    productTitle={product.name} 
+                                    price={product.price} 
+                                    discountPercent={product.discountPercentage} 
+                                    imgs={product.images} 
+                                    buyNowHandler={(e) => {e.preventDefault(); console.log("Buy Now Clicked")}} 
+                                    addToCartHandler={(e) => {
+                                        console.log("Add to Cart Clicked");
+                                    }}
+                                    onClickHandler={onProductCardClickHandler}
+                                />)
+                            })
+                        }
                     </div>
                 </div>
             )
